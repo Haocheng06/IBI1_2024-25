fcb11d64c42c250894b81015abe72ca4d6840794
Haocheng <haocheng.24@intl.zju.edu.cn>
2025-04-09 06:52:39 +0100

    Practical8

--- Practical8/Restriction_enzyme_cut_sites.py
+++ Practical8/Restriction_enzyme_cut_sites.py
@@ -3,12 +3,12 @@ def restriction_enzyme_cut(dna_sequence,enzyme_sequence):
     if re.search(r"[^ATCG]+",dna_sequence) or re.search(r"[^ATCG]+",enzyme_sequence):
         return("Your DNA sequence or Enzyme recognition ssequence is Wrong")
     cut_position=[]
-    for i in range(len(dna_sequence)-4):
-        current_sequence=dna_sequence[i:i+4]
+    for i in range(len(dna_sequence)-len(enzyme_sequence)+1):
+        current_sequence=dna_sequence[i:i+len(enzyme_sequence)]
         if current_sequence==enzyme_sequence:
             cut_position.append(str(i+1))
     if cut_position==[]:
-        return("Error. There is no ACGT")
+        return(f"Error. There is no {enzyme_sequence}")
     else:
         return(f"enzyme cut position is at {cut_position}")
     
